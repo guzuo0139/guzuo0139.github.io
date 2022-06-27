9c8f6ce55e21c7d951e7eec1e265b9c22e1939c4
guzuo0139 <987915974@qq.com>
2022-06-27 15:40:47 +0800

    Site updated: 2022-06-27 15:40:47

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "0123",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "FF9900",
+    "citySize": "16",
+    "aqiColor": "00FFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "left",
+    "horizontal": "left",
+    "left": "120",
+    "top": "10",
+    "key": "40102fe1c54845c489baff3417bdba2c"
+  }
+}
